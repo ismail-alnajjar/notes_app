fc5d325fefe0fe8969e0eff224773c76fd210547
ismail-alnajjar <ismaillalnajjaar@gmail.com>
2024-10-05 18:39:41 +0300

    Create Notes model

---
+++ lib/Models/NotesModel.dart
@@ -0,0 +1,12 @@
+class NotesModel {
+  final String title;
+  final String subtitle;
+  final String Data;
+  final int color;
+
+  NotesModel(
+      {required this.title,
+      required this.subtitle,
+      required this.Data,
+      required this.color});
+}
