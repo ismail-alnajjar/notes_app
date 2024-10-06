c1ba7efc68c6519c6a97f4862349b04a4810876a
ismail-alnajjar <ismaillalnajjaar@gmail.com>
2024-10-06 20:07:06 +0300

    add add notes state

--- lib/cubits/add_note_cubit/add_note_cubit_dart_state.dart
+++ lib/cubits/add_note_cubit/add_note_cubit_dart_state.dart
@@ -3,4 +3,14 @@ part of 'add_note_cubit_dart_cubit.dart';
 @immutable
 sealed class AddNoteCubitDartState {}
 
-final class AddNoteCubitDartInitial extends AddNoteCubitDartState {}
+final class AddNoteInitial extends AddNoteCubitDartState {}
+
+final class AddNoteLoading extends AddNoteCubitDartState {}
+
+final class AddNoteSuccess extends AddNoteCubitDartState {}
+
+final class AddNoteFailure extends AddNoteCubitDartState {
+  final String errMessage;
+
+  AddNoteFailure({required this.errMessage});
+}
